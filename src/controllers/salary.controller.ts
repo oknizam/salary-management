338ab7b57d8c0f0bbb50823e887250dac29e09ff
Nizamuddin Mohammed <nizamuddin.mohammed@nextiva.com>
2026-04-07 23:44:51 +0530

    endpoint added for get average, maximum and minimum salary by country

--- src/controllers/salary.controller.ts
+++ src/controllers/salary.controller.ts
@@ -23,3 +23,24 @@ export async function fetchGrossSalary(req: any, res: any) {
   }
 
 }
+
+export async function fetchSalariesByCountry(req: any, res: any) {
+  try {
+    const country = req.params.country;
+
+    if (!country) {
+      return res.status(400).json({ message: "please send country" })
+    }
+
+    const response = db.prepare(`SELECT avg(salary) as avgSalary, max(salary) as maxSalary, min(salary) as minSalary FROM employees WHERE country = ?`).get(country.toLowerCase())
+
+
+    return res.status(200).json(response);
+
+  }
+  catch (err) {
+    console.log("error in fething gross salary", err)
+    return res.status(500).json({ message: "Internal server error" })
+  }
+
+}
