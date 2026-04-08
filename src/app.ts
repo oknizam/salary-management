6621d548be8a5d4d1d11bc3d8ca781472233ab5d
Nizamuddin Mohammed <nizamuddin.mohammed@nextiva.com>
2026-04-08 15:38:29 +0530

    salary route added in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { initDB } from "./DB/dbConfig";
 import { employeeRoute } from "./routes/employee.route"
+import { salaryRoute } from "./routes/salary.routes";
 
 
 const app = express();
@@ -10,7 +11,9 @@ initDB();
 
 app.use(express.json());
 
-app.use('/employees', employeeRoute)
+app.use('/employees', employeeRoute);
+app.use('/salary', salaryRoute);
+
 
 
 app.listen(PORT, () => {
